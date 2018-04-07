da63dfadaaad6e2bdf99f7db7bf11cd27169fb9b
Keith Orlando <keith.a.orlando@gmail.com>
2018-04-07 01:22:13 -0400

    break up funcs, set username to lowercase for argon2

--- main.go
+++ main.go
@@ -16,13 +16,18 @@ import (
   _ "github.com/go-sql-driver/mysql"
 )
 
-const CredentialTable = "cred_hash_1_64_8_64_test"
-const PasswordDataTable = "pw_data_test"
+const CredentialTable = "cred_hash_1_64_8_64"
+const PasswordDataTable = "pw_data"
 
 const ParseFailed = "Parse"
 const CredInsertFailed = "CredInsert"
+const CredDeleteFailed = "CredDelete"
 const IncrementFailed = "Increment"
 const PwInsertFailed = "PwInsert"
+
+const DataPath = "./data/data.tsv"
+const FailuresFilePath = "./data/failures.txt"
+const TempFailuresFilePath = "./data/failures.tmp.txt"
 
 type failure struct {
   line string
@@ -30,8 +35,141 @@ type failure struct {
   err error
 }
 
+func analyzePassword(db *sql.DB, password string) (failure, error) {
+  var fail failure
+  rows, err := db.Query("SELECT count FROM " + PasswordDataTable + " WHERE pw=? LIMIT 1", password)
+  if err == nil {
+    defer rows.Close()
+    if rows.Next() {
+      _, err := db.Exec("UPDATE " + PasswordDataTable + " SET count = count + 1 WHERE pw=?", password)
+      if err != nil {
+        return failure{password, IncrementFailed, err}, err
+      }
+    } else {
+      length := len(password)
+      hasLower, _ := regexp.MatchString("[a-z]", password)
+      hasUpper, _ := regexp.MatchString("[A-Z]", password)
+      hasNumbers, _ := regexp.MatchString("[0-9]", password)
+      hasSymbols, _ := regexp.MatchString("[^a-zA-Z0-9]", password)
+      var low int
+      var up int
+      var num int
+      var sym int
+      if hasLower {
+        low = 1
+      }
+      if hasUpper {
+        up = 1
+      }
+      if hasNumbers {
+        num = 1
+      }
+      if hasSymbols {
+        sym = 1
+      }
+      _, err := db.Exec("INSERT INTO " + PasswordDataTable + " (pw, count, len, low, up, num, sym) VALUES (?, 1, ?, ?, ?, ?, ?)", password, length, low, up, num, sym)
+      if err != nil {
+        return failure{password, PwInsertFailed, err}, err
+      }
+    }
+  }
+  return fail, nil
+}
+
+func checkDB(db *sql.DB) {
+  err := db.Ping()
+  if err != nil {
+    log.Fatal(err)
+  }
+}
+
+func cleanUpFailures(path string) {
+  failureFile, err := os.Open(path)
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer failureFile.Close()
+  newFile, err := os.Create(TempFailuresFilePath)
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer newFile.Close()
+  scanner := bufio.NewScanner(failureFile)
+  writer := bufio.NewWriter(newFile)
+  for scanner.Scan() {
+    line := scanner.Text()
+    vals := strings.Split(line, "\t")
+    isDupeEntry, _ := regexp.MatchString("^Error 1062: Duplicate entry.+$", vals[1])
+    if vals[0] != CredInsertFailed || (vals[0] == CredInsertFailed && !isDupeEntry) {
+      writer.WriteString(line + "\n")
+    }
+  }
+  writer.Flush()
+  failureFile.Close()
+  newFile.Close()
+  err = os.Rename(TempFailuresFilePath, path)
+  if err != nil {
+    log.Fatal(err)
+  }
+}
+
+func correctUpperCase(db *sql.DB, limit, offset int) ([]failure, error) {
+  failures := []failure{}
+  file, err := os.Open(DataPath)
+  if err != nil {
+    return failures, err
+  }
+  defer file.Close()
+  scanner := bufio.NewScanner(file)
+  lineCount := 0
+  for (limit < 0 || lineCount < limit + offset) && scanner.Scan() {
+    lineCount += 1
+    if lineCount < offset {
+      continue
+    }
+    line := strings.TrimSpace(scanner.Text())
+    username, password, err := parseCredential(line)
+    if err == nil {
+      matched, err := regexp.MatchString("[A-Z]", username)
+      if err == nil && matched {
+        // remove the upper case version
+        upperCredHash, _ := runArgon2id([]byte(password), []byte(username), 1, 64*1024, 8, 64)
+        _, err := db.Exec("DELETE FROM " + CredentialTable + " WHERE hash=?", upperCredHash)
+        if err != nil {
+          failures = append(failures, failure{line, CredDeleteFailed, err})
+        }
+        // insert the lower case version
+        lowerCredHash, _ := runArgon2id([]byte(password), []byte(strings.ToLower(username)), 1, 64*1024, 8, 64)
+        _, err = db.Exec("INSERT INTO " + CredentialTable + " (hash) VALUES (?)", lowerCredHash)
+        if err != nil {
+          failures = append(failures, failure{line, CredInsertFailed, err})
+        }
+      }
+    }
+  }
+  return failures, nil
+}
+
+func doEncryption(db *sql.DB, limit, offset int) {
+  start := time.Now()
+  totalEncryptionTime, numEncryptions, failures, err := encryptAndInsertAll(db, DataPath, limit, offset)
+  if err != nil {
+    log.Fatal(err)
+  }
+  // totalEncryptionTime, numEncryptions := readAllFiles("./data", 5)
+  fmt.Println(numEncryptions, "credentials read, encrypted, and inserted in", time.Since(start))
+  fmt.Println("Number of failures:", len(failures))
+  avgDur, err := time.ParseDuration(strconv.FormatInt(totalEncryptionTime / int64(numEncryptions), 10) + "ns")
+  if err != nil {
+    log.Fatal(err)
+  }
+  fmt.Println("Average argon2id run time:", avgDur)
+  writeFailures(failures, FailuresFilePath)
+}
+
 // set limit to -1 (or anything < 0) to read all lines
 func encryptAndInsertAll(db *sql.DB, path string, limit, offset int) (int64, int, []failure, error) {
+  start := time.Now()
   totalEncryptionTime := int64(0)
   numEncryptions := 0
   failures := []failure{}
@@ -52,43 +190,23 @@ func encryptAndInsertAll(db *sql.DB, path string, limit, offset int) (int64, int
     if err != nil {
       failures = append(failures, failure{line, ParseFailed, err})
     } else {
-      credHash, execTime := runArgon2id([]byte(password), []byte(username), 1, 64*1024, 8, 64)
+      credHash, execTime := runArgon2id([]byte(password), []byte(strings.ToLower(username)), 1, 64*1024, 8, 64)
       totalEncryptionTime += execTime.Nanoseconds()
       numEncryptions += 1
       _, err := db.Exec("INSERT INTO " + CredentialTable + " (hash) VALUES (?)", credHash)
       if err != nil {
         failures = append(failures, failure{line, CredInsertFailed, err})
       }
-      rows, err := db.Query("SELECT count FROM " + PasswordDataTable + " WHERE pw=? LIMIT 1", password)
+      fail, err := analyzePassword(db, password)
+      if err != nil {
+        failures = append(failures, fail)
+      }
+    }
+    if numEncryptions > 0 && numEncryptions % 10000 == 0 {
+      fmt.Println(numEncryptions, "credentials read, encrypted, and inserted in", time.Since(start), "so far")
+      avgDur, err := time.ParseDuration(strconv.FormatInt(totalEncryptionTime / int64(numEncryptions), 10) + "ns")
       if err == nil {
-        if rows.Next() {
-          _, err := db.Exec("UPDATE " + PasswordDataTable + " SET count = count + 1 WHERE pw=?", password)
-          if err != nil {
-            failures = append(failures, failure{line, IncrementFailed, err})
-          }
-        } else {
-          length := len(password)
-          hasLetters, _ := regexp.MatchString("[a-zA-Z]", password)
-          hasNumbers, _ := regexp.MatchString("[0-9]", password)
-          hasSymbols, _ := regexp.MatchString("[^a-zA-Z0-9]", password)
-          var let int
-          var num int
-          var sym int
-          if hasLetters {
-            let = 1
-          }
-          if hasNumbers {
-            num = 1
-          }
-          if hasSymbols {
-            sym = 1
-          }
-          _, err := db.Exec("INSERT INTO " + PasswordDataTable + " (pw, count, len, let, num, sym) VALUES (?, 1, ?, ?, ?, ?)", password, length, let, num, sym)
-          if err != nil {
-            failures = append(failures, failure{line, PwInsertFailed, err})
-          }
-        }
-        rows.Close()
+        fmt.Println("Average argon2id run time so far:", avgDur)
       }
     }
   }
@@ -186,40 +304,19 @@ func searchHash(db *sql.DB, hash []byte) (bool, error) {
   return false, nil
 }
 
-func main() {
-  db, err := getDB()
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer db.Close()
-  err = db.Ping()
-  if err != nil {
-    log.Fatal(err)
-  }
-  start := time.Now()
-  totalEncryptionTime, numEncryptions, failures, err := encryptAndInsertAll(db, "./data/data.tsv", 500, 0)
-  if err != nil {
-    log.Fatal(err)
-  }
-  // totalEncryptionTime, numEncryptions := readAllFiles("./data", 5)
-  fmt.Println(numEncryptions, "credentials read, encrypted, and inserted in", time.Since(start))
-  avgDur, err := time.ParseDuration(strconv.FormatInt(totalEncryptionTime / int64(numEncryptions), 10) + "ns")
-  fmt.Println("Number of failures:", len(failures))
-  if err != nil {
-    log.Fatal(err)
-  }
-  fmt.Println("Average argon2id run time:", avgDur)
+func writeFailures(failures []failure, path string) {
   if len(failures) > 0 {
-    var f *os.File
+    var file *os.File
     var err error
-    f, err = os.OpenFile("./data/failures.txt", os.O_APPEND, 0644)
+    file, err = os.OpenFile(path, os.O_APPEND, 0644)
     if err != nil {
-      f, err = os.Create("./data/failures.txt")
+      file, err = os.Create(path)
       if err != nil {
         log.Fatal(err)
       }
     }
-    w := bufio.NewWriter(f)
+    defer file.Close()
+    w := bufio.NewWriter(file)
     for _, failure := range failures {
       _, err := w.WriteString(failure.desc + "\t" + failure.err.Error() + "\t" + failure.line + "\n")
       if err != nil {
@@ -228,4 +325,20 @@ func main() {
     }
     w.Flush()
   }
+}
+
+func main() {
+  db, err := getDB()
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer db.Close()
+  checkDB(db)
+  // cleanUpFailures(FailuresFilePath)
+  doEncryption(db, 500000, 2500000)
+  // failures, err := correctUpperCase(db, 2500000, 0)
+  // if err != nil {
+  //   log.Fatal(err)
+  // }
+  // writeFailures(failures, "./data/uppercase-correction-failures.txt")
 }
